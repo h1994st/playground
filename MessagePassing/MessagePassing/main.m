7ae6ab1ceb3f7607930af15a81b3dbc4cee0b0a6
h1994st <h1994st@gmail.com>
2014-07-16 22:03:28 +0800

    init MessagePassing

---
+++ MessagePassing/MessagePassing/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MessagePassing
+//
+//  Created by Tom Hu on 7/16/14.
+//  Copyright (c) 2014 Tom Hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
