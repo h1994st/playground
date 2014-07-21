60e968f33158bb9fa219fbec81ac7aa8da41dc83
h1994st <h1994st@gmail.com>
2014-07-21 23:57:38 +0800

    quick sort

--- Python/qsort-3-line.py
+++ Python/qsort-3-line.py
@@ -4,12 +4,38 @@ def qsort(L):
 	if len(L) <= 1: return L  
 	return qsort([lt for lt in L[1:] if lt < L[0]]) + L[0:1] + qsort([ge for ge in L[1:] if ge >= L[0]])
 
-list = []
-for i in range(100):
-	list.append(random.randint(0, 10000))
+def qsort2(L):  
+    if not L:
+        return L
+    pivot = L[0]
 
-print list[1:]
-print [lt for lt in list[1:] if lt < list[0]]
+    def lt(x):
+        return x<pivot
+
+    def ge(x):
+        return x>=pivot
+
+    return qsort2(filter(lt, L[1:]))+[pivot]+qsort2(filter(ge, L[1:]))
+
+def qsort3(L):  
+    if not L:
+        return L  
+    pivot = random.choice(L)  
+
+    def lt(x):
+        return x<pivot
+
+    def gt(x):
+        return x>pivot
+
+    return qsort3(filter(lt, L))+[pivot]*L.count(pivot)+qsort3(filter(gt, L))
+
+list = range(1000)
+random.shuffle(list)
 
 print list
 print qsort(list)
+print list
+print qsort2(list)
+print list
+print qsort3(list)
