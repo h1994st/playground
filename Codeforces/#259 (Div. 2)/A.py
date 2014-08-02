6f7b2d8b6d86ca79e2a02e58c08ae8420e11a593
h1994st <h1994st@gmail.com>
2014-08-02 23:38:17 +0800

    codeforces Round #259 (Div. 2) A

--- Codeforces/#259 (Div. 2)/A.py
+++ Codeforces/#259 (Div. 2)/A.py
@@ -1,10 +1,10 @@
 def main():
     n = int(raw_input())
 
-    mid = n / 2 + 1
-
     for x in range(n):
-        
+        a = min(x, n - x - 1) * 2 + 1
+        b = (n - a) / 2
+        print '*' * b + 'D' * a + '*' * b
 
 if __name__ == '__main__':
     main()
