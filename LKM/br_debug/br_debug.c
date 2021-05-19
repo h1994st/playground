53db486374a38734846b29dec201fabeb9871b34
h1994st <h1994st@gmail.com>
2021-05-19 16:39:00 -0400

    br_debug: add two inet hooks and disable hooking in the root netns

--- LKM/br_debug/br_debug.c
+++ LKM/br_debug/br_debug.c
@@ -8,12 +8,85 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <linux/kallsyms.h>
 
-static unsigned int br_pre_routing_hookfn(void *priv,
-                                          struct sk_buff *skb,
-                                          const struct nf_hook_state *state)
+#if !IS_ENABLED(CONFIG_BRIDGE_NETFILTER)
+#warning "BRIDGE_NETFILTER is disabled!"
+#endif
+
+#if IS_ENABLED(CONFIG_BRIDGE_NETFILTER)
+static unsigned int br_pre_routing_first_hookfn(void *priv,
+                                                struct sk_buff *skb,
+                                                const struct nf_hook_state *state)
 {
-    pr_info("NF_BR_PRE_ROUTING\n");
+    const struct nf_hook_entry *hook;
+    struct nf_hook_entries *hooks = NULL;
+    struct net *net;
+    int i;
+
+    pr_info("NF_BR_PRE_ROUTING first\n");
+    pr_info("skb=%px: len=%u headroom=%u head=%px data=%px, tail=%u, end=%u\n",
+            skb, skb->len, skb_headroom(skb), skb->head, skb->data, skb->tail, skb->end);
+
+    net = dev_net(skb->dev);
+    hooks = rcu_dereference(net->nf.hooks_bridge[NF_BR_PRE_ROUTING]);
+    if (!hooks)
+        goto out;
+
+    pr_info("#hooks of NF_BR_PRE_ROUTING: %u\n", hooks->num_hook_entries);
+    for (i = 0; i < hooks->num_hook_entries; ++i) {
+        hook = &hooks->hooks[i];
+        pr_info("hook %d: %pF\n", i, hook->hook);
+    }
+
+out:
+    return NF_ACCEPT;
+}
+
+static unsigned int br_pre_routing_last_hookfn(void *priv,
+                                               struct sk_buff *skb,
+                                               const struct nf_hook_state *state)
+{
+    pr_info("NF_BR_PRE_ROUTING last\n");
+    pr_info("skb=%px: len=%u headroom=%u head=%px data=%px, tail=%u, end=%u\n",
+            skb, skb->len, skb_headroom(skb), skb->head, skb->data, skb->tail, skb->end);
+
+    return NF_ACCEPT;
+}
+
+static unsigned int inet_pre_routing_first_hookfn(void *priv,
+                                                  struct sk_buff *skb,
+                                                  const struct nf_hook_state *state)
+{
+    const struct nf_hook_entry *hook;
+    struct nf_hook_entries *hooks = NULL;
+    struct net *net;
+    int i;
+
+    pr_info("NF_INET_PRE_ROUTING first\n");
+    pr_info("skb=%px: len=%u headroom=%u head=%px data=%px, tail=%u, end=%u\n",
+            skb, skb->len, skb_headroom(skb), skb->head, skb->data, skb->tail, skb->end);
+
+    net = dev_net(skb->dev);
+    hooks = rcu_dereference(net->nf.hooks_ipv4[NF_INET_PRE_ROUTING]);
+    if (!hooks)
+        goto out;
+
+    pr_info("#hooks of NF_INET_PRE_ROUTING: %u\n", hooks->num_hook_entries);
+    for (i = 0; i < hooks->num_hook_entries; ++i) {
+        hook = &hooks->hooks[i];
+        pr_info("hook %d: %pF\n", i, hook->hook);
+    }
+
+out:
+    return NF_ACCEPT;
+}
+
+static unsigned int inet_pre_routing_last_hookfn(void *priv,
+                                                 struct sk_buff *skb,
+                                                 const struct nf_hook_state *state)
+{
+    pr_info("NF_INET_PRE_ROUTING last\n");
     pr_info("skb=%px: len=%u headroom=%u head=%px data=%px, tail=%u, end=%u\n",
             skb, skb->len, skb_headroom(skb), skb->head, skb->data, skb->tail, skb->end);
 
@@ -59,13 +132,33 @@ static unsigned int br_post_routing_hookfn(void *priv,
             skb, skb->len, skb_headroom(skb), skb->head, skb->data, skb->tail, skb->end);
     return NF_ACCEPT;
 }
+#endif
 
 static const struct nf_hook_ops br_debug_nf_ops[] = {
+#if IS_ENABLED(CONFIG_BRIDGE_NETFILTER)
     {
-        .hook = br_pre_routing_hookfn,
+        .hook = br_pre_routing_first_hookfn,
         .pf = NFPROTO_BRIDGE,
         .hooknum = NF_BR_PRE_ROUTING,
         .priority = NF_BR_PRI_FIRST,
+    },
+    {
+        .hook = br_pre_routing_last_hookfn,
+        .pf = NFPROTO_BRIDGE,
+        .hooknum = NF_BR_PRE_ROUTING,
+        .priority = NF_BR_PRI_LAST,
+    },
+    {
+        .hook = inet_pre_routing_first_hookfn,
+        .pf = NFPROTO_IPV4,
+        .hooknum = NF_INET_PRE_ROUTING,
+        .priority = NF_BR_PRI_FIRST,
+    },
+    {
+        .hook = inet_pre_routing_last_hookfn,
+        .pf = NFPROTO_IPV4,
+        .hooknum = NF_INET_PRE_ROUTING,
+        .priority = NF_BR_PRI_LAST,
     },
     {
         .hook = br_local_in_hookfn,
@@ -91,16 +184,26 @@ static const struct nf_hook_ops br_debug_nf_ops[] = {
         .hooknum = NF_BR_POST_ROUTING,
         .priority = NF_BR_PRI_FIRST,
     },
+#endif
 };
 
 static int __net_init br_debug_nf_register(struct net *net)
 {
+    if (net_eq(net, &init_net)) {
+        // do not hook the root network namespace
+        return 0;
+    }
+
     return nf_register_net_hooks(net, br_debug_nf_ops,
                                  ARRAY_SIZE(br_debug_nf_ops));
 }
 
 static void __net_exit br_debug_nf_unregister(struct net *net)
 {
+    if (net_eq(net, &init_net)) {
+        // no hook for the root network namespace
+        return;
+    }
     nf_unregister_net_hooks(net, br_debug_nf_ops,
                             ARRAY_SIZE(br_debug_nf_ops));
 }
